abf6ace430d392012e4b572c5c46a6233e275090
giuliano.iacobelli <giuliano.iacobelli@gmail.com>
2015-05-29 15:32:48 +0200

    fixing fb share

--- js/app.js
+++ js/app.js
@@ -55,14 +55,12 @@ $(document).ready(function(){
     $('#searchbar').hide();    
   })
 
-  $('#fbshare').on('click', function(e){
+  $('#fbshare').on('click', function(e){ 
       FB.ui({
-      method: 'share_open_graph',
-      action_type: 'og.likes',
-      action_properties: JSON.stringify({
-          object:'https://developers.facebook.com/docs/',
-      })
-    }, function(response){});
+        method: 'share',
+        href: urlToShare
+      }, function(response){});
   })
+      
 
 }); 
